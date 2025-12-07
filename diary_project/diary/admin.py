d4adba63d92454708f2b9e0ecc2f023d36a61b33
Vadym <vadymkravtcsov@gmail.com>
2025-12-07 21:56:40 +0200

    feat: add created admin panel

--- diary_project/diary/admin.py
+++ diary_project/diary/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Tag, Entry
 # Register your models here.
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "created"]
+    search_fields = ["title", "content"]
+    list_filter = ["created", "tags"]
+    ordering = ["-created"]
+
+@admin.register(Tag)
+class TagAdmon(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
